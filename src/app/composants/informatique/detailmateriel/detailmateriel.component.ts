1730d452ea4fbe7ea97120b2bf14cb308bda5342
Amel TRIKI <amel.triki.iset@gmail.com>
2020-12-24 09:24:58 +0100

    Ajout d'un service

--- src/app/composants/informatique/detailmateriel/detailmateriel.component.ts
+++ src/app/composants/informatique/detailmateriel/detailmateriel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Materiel } from 'src/app/models/materiel';
+import { MaterielService } from 'src/app/services/materiel.service';
 
 @Component({
   selector: 'app-detailmateriel',
@@ -9,28 +10,15 @@ import { Materiel } from 'src/app/models/materiel';
 export class DetailmaterielComponent implements OnInit {
 
   afficher:boolean =false;
-  materiel:Materiel =  {   
-    id: '2', 
-    libelle: 'PC', 
-    photo:'assets/Images/Informatique/pcFixe.jpg',
-    marque: 'HP', 
-    prix: 1780.6, 
-    description: 'PC Portable très pratique', 
-    hautGamme: false,
-    quantite: 5,
-    commentaires:[ 
-      {contenu:' satisfaisant', note:3, auteur:'Sami MRAD', date:new Date(2020,11,4)},
-
-    {contenu:'Bon rapport qualité prix', note:4, auteur:'Samar Salah', date:new Date(2020,10,4)}
-  ]
-};
+  materiel:Materiel ;
 /*
 onClick(){
   this.afficher = !this.afficher;
 }*/
-  constructor() { }
+  constructor(private materielService:MaterielService) { }
 
   ngOnInit(): void {
+    this.materiel = this.materielService.getMaterielById("3");
   }
 
 }
